3ffecd4e5ff1828422c44f515a3020d2da5c8b97
santiagoVL03 <santiagovl0308@gmail.com>
2024-07-28 18:00:56 -0500

    testing_services_template

---
+++ src/main/java/com/dseproj/qaliservices/QaliservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dseproj.qaliservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaliservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaliservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dseproj/qaliservices/QaliservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dseproj.qaliservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaliservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
