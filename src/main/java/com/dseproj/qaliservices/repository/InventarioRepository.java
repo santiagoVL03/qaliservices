02bc659a72228c54e28999324c555ab39cb767eb
Eduardo Ruiz <eruizm@unsa.edu.pe>
2024-08-02 06:09:55 -0500

    Improve architecture names

--- src/main/java/com/dseproj/qaliservices/repository/InventarioRepository.java
+++ src/main/java/com/dseproj/qaliservices/repository/InventarioRepository.java
@@ -3,7 +3,7 @@ package com.dseproj.qaliservices.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dseproj.qaliservices.model.InventarioEntity;
+import com.dseproj.qaliservices.domain.InventarioEntity;
 @Repository
 public interface InventarioRepository extends CrudRepository<InventarioEntity, Integer> {
     
